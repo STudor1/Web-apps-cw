fbec0cd4134a4483549b14b040f852925550c8cc
Tudor <tudor122stefan@gmail.com>
2022-12-10 16:04:34 +0000

    Keep the old value when a mistake is made

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -5,7 +5,8 @@
 @section('content')
     <form method="POST" action="{{ route('users.store') }}">
         @csrf
-        <p>Content: <input type="text" name="name"></p>
+        <p>Content: <input type="text" name="name" 
+            value="{{ old('name') }}"></p>
 
         <input type="submit" value="Submit">
         <a href="{{ route('users.index') }}">Cancel</a>
