165e0a3950cf593e458444bacd5d6e2d35d3a6ea
Tudor <tudor122stefan@gmail.com>
2022-12-12 16:47:40 +0000

    Can upload image

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -5,12 +5,12 @@
 @section('content')
     <form method="POST" action="{{ route('users.store') }}">
         @csrf
-        <p>Image Link: <input type="text" name="image" 
-            value="{{ old('image') }}"></p>
         <p>Title: <input type="text" name="title" 
             value="{{ old('title') }}"></p>
         <p>Content: <input type="text" name="content" 
             value="{{ old('content') }}"></p>
+        <p>Upload an image <input type="file" name="image" 
+            value="{{ old('image') }}"></p>
 
         <input type="submit" value="Submit">
         <a href="{{ route('users.index') }}" class="btn btn-primary">Cancel</a>
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -6,12 +6,12 @@
     <form method="POST" action="{{ route('users.update', [$post->id]) }}">
         @csrf
         @method('PUT')
-        <p>Image Link: <input type="text" name="image" 
-            value="{{ $post->image }}"></p>
         <p>Title: <input type="text" name="title" 
             value="{{ $post->title }}"></p>
         <p>Content: <input type="text" name="content" 
             value="{{ $post->content }}"></p>
+        <p>Upload an image <input type="file" name="image" 
+            value="{{ $post->image }}"></p>
 
         <input type="submit" value="Submit">
         <a href="{{ route('users.show', [$post->id]) }}" class="btn btn-primary">Cancel</a>
