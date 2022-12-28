7253fc514e4b2ef0314ace7e4f8b553919847b82
Tudor <tudor122stefan@gmail.com>
2022-12-28 15:49:57 +0000

    Changed how posts are displayed

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,15 +3,20 @@
 @section('title', 'Home Page')
     
 @section('content')
+    <div class="card w-100" style="width: 18rem;">
+        <div class="card-header">
+            Posts:
+        </div>
+        <ul class="list-group list-group-flush">
+            @foreach ($posts as $post)
+                <div class="card">
+                    <li class="list-group-item"><a href="{{ route('users.show', [$post->id]) }}">{{ $post->title }}</a></li>
+                </div>
+            @endforeach
+        
+        </ul>
+    </div>
     
-
-    <p> Posts: </p>
-
-    <ul>
-        @foreach ($posts as $post)
-            <li><a href="{{ route('users.show', [$post->id]) }}">{{ $post->title }}</a></li>
-        @endforeach
-    </ul>
 
     <a href="{{ route('users.create') }}" class="btn btn-primary">Create Post </a>
 
