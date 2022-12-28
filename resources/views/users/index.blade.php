bb69e6e25483692511df4ff8c470a8a17bac5788
Tudor <tudor122stefan@gmail.com>
2022-12-28 15:22:18 +0000

    Set up a navbar

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,27 +1,47 @@
 @extends('layouts.basic')
-
-<!-- Responsive Settings Options -->
-<div class="pt-4 pb-1 border-t border-gray-200">
-    <div class="px-4">
-        <div class="font-medium text-base text-gray-800">User: {{ Auth::user()->name }}</div>
-        <div class="font-medium text-sm text-gray-500">Email: {{ Auth::user()->email }}</div>
-        <!-- HERE FOR TESTING -->
-        <div class="font-medium text-sm text-gray-500">ID: {{ Auth::user()->id }}</div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Profile</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                {{--
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                --}}
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    User Details
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li> &nbsp;&nbsp; User: {{ Auth::user()->name }}</li>
+                        <li> &nbsp;&nbsp; {{ Auth::user()->email }}</li>
+                        <li> &nbsp;&nbsp; TESTING ID: {{ Auth::user()->id }}</li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                    
+                                <x-responsive-nav-link :href="route('logout')"
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                    {{ __('Log Out') }}
+                                </x-responsive-nav-link>
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
     </div>
-
-    <div class="mt-3 space-y-1">
-        <!-- Authentication -->
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-
-            <x-responsive-nav-link :href="route('logout')"
-                    onclick="event.preventDefault();
-                                this.closest('form').submit();">
-                {{ __('Log Out') }}
-            </x-responsive-nav-link>
-        </form>
-    </div>
-</div>
+</nav>
 
 @section('title', 'Home Page')
     
