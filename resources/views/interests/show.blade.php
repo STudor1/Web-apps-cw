b94e89d6f9c53b9cfe7f2d418d6b45bed0ad6c11
Tudor <tudor122stefan@gmail.com>
2022-12-31 15:48:01 +0000

    Fixed a bug and found out where the 1 comes from

--- resources/views/interests/show.blade.php
+++ resources/views/interests/show.blade.php
@@ -3,13 +3,15 @@
 @section('title', $interest->interest)
 
 @section('content')
-    
+    {{--this prints 1 cause it is true no idea how to fix it--}}
+    {{$interested = true}}
     Users interested in {{$interest->interest}}:
     @foreach ($interest->users as $user)
         {{--<li> {{$user->pivot->user_id}}</li>--}}
         <li> {{$user->name}} </li>
         
         @if ($id_user == $user->id)
+            {{--this prints 1 cause it is true no idea how to fix it--}}    
             {{$interested = true}}
         @else
             {{$interested = false}}
